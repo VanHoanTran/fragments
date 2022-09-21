966f830d620ff0ebee688aecf3acc812c5efdf3c
VanHoanTran <tvan-hoan@myseneca.ca>
2022-09-20 23:35:46 -0400

    revise the parserOption

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,22 +16,3 @@ module.exports = {
   },
   rules: {},
 };
-
-
-// .eslintrc.js
-
-// module.exports = {
-//   env: {
-//     node: true,
-//     commonjs: true,
-//     es2021: true,
-//     // Add this next line to configure ESLint for Jest, see:
-//     // https://eslint.org/docs/user-guide/configuring/language-options#specifying-environments
-//     jest: true,
-//   },
-//   extends: 'eslint:recommended',
-//   parserOptions: {
-//     ecmaVersion: 13,
-//   },
-//   rules: {},
-// };
