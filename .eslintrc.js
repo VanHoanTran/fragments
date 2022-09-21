e19d502323df338bc9e936686abf16855b263164
VanHoanTran <tvan-hoan@myseneca.ca>
2022-09-20 23:32:38 -0400

    install and configure Jest for testing

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,9 @@ module.exports = {
     node: true,
     commonjs: true,
     es2021: true,
+    // Add this next line to configure ESLint for Jest, see:
+    // https://eslint.org/docs/user-guide/configuring/language-options#specifying-environments
+    jest: true,
   },
   extends: 'eslint:recommended',
   overrides: [],
@@ -13,3 +16,22 @@ module.exports = {
   },
   rules: {},
 };
+
+
+// .eslintrc.js
+
+// module.exports = {
+//   env: {
+//     node: true,
+//     commonjs: true,
+//     es2021: true,
+//     // Add this next line to configure ESLint for Jest, see:
+//     // https://eslint.org/docs/user-guide/configuring/language-options#specifying-environments
+//     jest: true,
+//   },
+//   extends: 'eslint:recommended',
+//   parserOptions: {
+//     ecmaVersion: 13,
+//   },
+//   rules: {},
+// };
