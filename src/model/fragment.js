bd633acbf8b490a6550025638e8488f5e81fcb58
VanHoanTran <tvan-hoan@myseneca.ca>
2022-10-05 08:27:42 -0400

    add a Fragment Class to store data and metadata layer 3

---
+++ src/model/fragment.js
@@ -0,0 +1,161 @@
+// Use crypto.randomUUID() to create unique IDs, see:
+// https://nodejs.org/api/crypto.html#cryptorandomuuidoptions
+const { randomUUID } = require('crypto');
+// Use https://www.npmjs.com/package/content-type to create/parse Content-Type headers
+const contentType = require('content-type');
+
+const validTypes = [
+  `text/plain`,
+  /*
+   Currently, only text/plain is supported. Others will be added later.
+  `text/markdown`,
+  `text/html`,
+  `application/json`,
+  `image/png`,
+  `image/jpeg`,
+  `image/webp`,
+  `image/gif`,
+  */
+];
+
+// Functions for working with fragment metadata/data using our DB
+const {
+  readFragment,
+  writeFragment,
+  readFragmentData,
+  writeFragmentData,
+  listFragments,
+  deleteFragment,
+} = require('./data');
+
+class Fragment {
+  constructor({ id, ownerId, created, updated, type, size = 0 }) {
+    if(!type || !ownerId){
+      throw new Error(`ownerId and type are required, got type=${type}, got ownerId=${ownerId}`);
+    }
+
+    if(!Fragment.isSupportedType(type)){
+      throw new Error(`unsupported type, got type=${type}`);
+    }
+
+    if(typeof size != 'number' || size < 0){
+      throw new Error(`size must be 0 or a positive number, got size=${size}`);
+    }
+
+
+
+    this.ownerId = ownerId;
+    this.type = type;
+    this.id = id != undefined ? id : randomUUID();
+    this.created = created || new Date().toISOString();
+    this.updated = updated || new Date().toISOString();
+    this.size = size;
+  }
+
+  /**
+   * Get all fragments (id or full) for the given user
+   * @param {string} ownerId user's hashed email
+   * @param {boolean} expand whether to expand ids to full fragments
+   * @returns Promise<Array<Fragment>>
+   */
+  static async byUser(ownerId, expand = false) {
+    // TODO
+    const fragments = await listFragments(ownerId, expand);
+    return fragments;
+  }
+
+  /**
+   * Gets a fragment for the user by the given id.
+   * @param {string} ownerId user's hashed email
+   * @param {string} id fragment's id
+   * @returns Promise<Fragment>
+   */
+  static async byId(ownerId, id) {
+    // TODO
+    const fragment = await readFragment(ownerId, id);
+    if (!fragment) 
+      throw new Error(`User ownerId =${ownerId} Not Found!`);
+    return fragment;
+  }
+
+  /**
+   * Delete the user's fragment data and metadata for the given id
+   * @param {string} ownerId user's hashed email
+   * @param {string} id fragment's id
+   * @returns Promise
+   */
+  static delete(ownerId, id) {
+    // TODO
+    return deleteFragment(ownerId, id);
+  }
+
+  /**
+   * Saves the current fragment to the database
+   * @returns Promise
+   */
+  save() {
+    // TODO
+    this.updated = new Date().toISOString();
+    writeFragment(this);
+  }
+
+  /**
+   * Gets the fragment's data from the database
+   * @returns Promise<Buffer>
+   */
+  getData() {
+    return readFragmentData(this.ownerId, this.id)
+  }
+
+  /**
+   * Set's the fragment's data in the database
+   * @param {Buffer} data
+   * @returns Promise
+   */
+  async setData(data) {
+    // TODO
+    this.updated = new Date().toISOString();
+    this.size = data.byteLength;
+    await writeFragmentData(this.ownerId, this.id, data)
+  }
+
+  /**
+   * Returns the mime type (e.g., without encoding) for the fragment's type:
+   * "text/html; charset=utf-8" -> "text/html"
+   * @returns {string} fragment's mime type (without encoding)
+   */
+  get mimeType() {
+    const { type } = contentType.parse(this.type);
+    return type;
+  }
+
+  /**
+   * Returns true if this fragment is a text/* mime type
+   * @returns {boolean} true if fragment's type is text/*
+   */
+  get isText() {
+    return this.mimeType.startsWith('text/');
+  }
+
+  /**
+   * Returns the formats into which this fragment type can be converted
+   * @returns {Array<string>} list of supported mime types
+   */
+  get formats() {
+    // TODO
+    return validTypes;
+  }
+
+  /**
+   * Returns true if we know how to work with this content type
+   * @param {string} value a Content-Type value (e.g., 'text/plain' or 'text/plain: charset=utf-8')
+   * @returns {boolean} true if we support this Content-Type (i.e., type/subtype)
+   */
+  static isSupportedType(value) {
+    // TODO
+    const { type } = contentType.parse(value);
+    return validTypes.includes(type);
+  }
+}
+
+module.exports.Fragment = Fragment;
