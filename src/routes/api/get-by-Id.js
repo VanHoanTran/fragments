797f3958a6f12395d03cc24ff3a4b80b19e96b2f
VanHoanTran <tvan-hoan@myseneca.ca>
2022-10-08 23:47:05 -0400

    change return type of getbyid

--- src/routes/api/get-by-Id.js
+++ src/routes/api/get-by-Id.js
@@ -1,16 +1,14 @@
 const logger = require('../../logger');
 const { Fragment } = require('../../model/fragment');
-const { createSuccessResponse, createErrorResponse } = require('../../response');
+const { createErrorResponse } = require('../../response');
 module.exports = async (req, res) => {
   try {
+    logger.debug(`ownerId=${req.user}, id=${req.params.id}`);
     const fragment = await Fragment.byId(req.user, req.params.id);
-    res.status(200).json(
-      createSuccessResponse({
-        fragment: fragment,
-      })
-    );
+    res.setHeader('Content-Type', fragment.type);
+    fragment.getData().then((data) => res.status(200).send(data));
   } catch (error) {
     logger.debug(`ownerId=${req.user}, id=${req.params.id}`);
-    return res.status(404).json(createErrorResponse(404, 'The fragment does not exist'));
+    res.status(404).json(createErrorResponse(404, 'The fragment does not exist'));
   }
 };
--- tests/unit/get-by-Id.test.js
+++ tests/unit/get-by-Id.test.js
@@ -3,7 +3,6 @@ const request = require('supertest');
 const app = require('../../src/app');
 
 describe('GET /v1/fragments/:id', () => {
-
   //requests with authentication will get HTTP 200 response
   test('should return HTTP 200 response', async () => {
     const postRes = await request(app)
@@ -14,11 +13,9 @@ describe('GET /v1/fragments/:id', () => {
 
     const id = JSON.parse(postRes.text).fragment.id;
 
-    const res = await request(app)
-      .get(`/v1/fragments/${id}`)
-      .auth('user1@email.com', 'password1');
-    
-      expect(res.statusCode).toBe(200);
+    const res = await request(app).get(`/v1/fragments/${id}`).auth('user1@email.com', 'password1');
+
+    expect(res.statusCode).toBe(200);
   });
 
   test(`Accessing posted fragment should return back the same fragment`, async () => {
@@ -28,13 +25,10 @@ describe('GET /v1/fragments/:id', () => {
       .set('Content-Type', 'text/plain')
       .send('A plain text fragment');
     const id = JSON.parse(postRes.text).fragment.id;
-    const fragment = JSON.parse(postRes.text).fragment;
 
-    const res = await request(app)
-      .get(`/v1/fragments/${id}`)
-      .auth('user2@email.com', 'password2');
+    const res = await request(app).get(`/v1/fragments/${id}`).auth('user2@email.com', 'password2');
 
-    expect(res.body.fragment).toEqual(fragment);
+    expect(res.text).toEqual('A plain text fragment');
   });
 
   // Can not access other's fragments even authenticated users
@@ -45,13 +39,11 @@ describe('GET /v1/fragments/:id', () => {
       .set('Content-Type', 'text/plain')
       .send('A plain text fragment');
     const id = JSON.parse(postRes.text).fragment.id;
-    const res = await request(app)
-      .get(`/v1/fragments/${id}`)
-      .auth('user2@email.com', 'password2');
+    const res = await request(app).get(`/v1/fragments/${id}`).auth('user2@email.com', 'password2');
 
     expect(res.statusCode).toBe(404);
   });
-    //requests without authentication are rejected with code 401
+  //requests without authentication are rejected with code 401
   test('requests without authentication should return HTTP 401 response', () =>
     request(app).get('/v1/fragments/id').expect(401));
 
