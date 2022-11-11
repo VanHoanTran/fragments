514211d3b51e958359e95c282466ee0bdadf6671
VanHoanTran <tvan-hoan@myseneca.ca>
2022-11-11 07:19:01 -0500

    add logic for converting data

--- src/routes/api/get-by-id.js
+++ src/routes/api/get-by-id.js
@@ -1,16 +1,51 @@
 const logger = require('../../logger');
 const { Fragment } = require('../../model/fragment');
 const { createErrorResponse } = require('../../response');
+//https://www.npmjs.com/package/mime-types
+const mime = require('mime-types');
 module.exports = async (req, res) => {
   try {
-    logger.debug(`ownerId=${req.user}, id=${req.params.id}`);
-    const fragment = await Fragment.byId(req.user, req.params.id);
+    const idExt = req.params.id.split('.');
+    const id = idExt[0];
+    const ext = idExt[1]; //.md, .txt, .json, etc.
+    logger.debug({ ownerId: req.user, fragmentsId: id, ext: ext }, 'Provided information: ');
+    const fragment = await Fragment.byId(req.user, id);
+    let data;
+    if (!idExt[1]) {
+      data = await fragment.getData();
+      // keep content type as fragment's type
+      res.setHeader('Content-Type', fragment.type);
+    } else {
+      const mimeType = mime.lookup(ext);
+      logger.debug({ mimeType: mimeType }, 'mimeType: ');
+      logger.debug({ formats: fragment.formats }, 'Convertible types: ');
+      if (fragment.formats.includes(mimeType)) {
+        data = await fragment.convertData(mimeType);
+        // set content type to desiredType
+        res.setHeader('Content-Type', mimeType);
+        logger.debug(
+          { originalType: fragment.mimeType, desiredType: mimeType },
+          'Type conversion succeeded: '
+        );
+      } else {
+        logger.debug(
+          { originalType: fragment.mimeType, desiredType: mimeType },
+          'Type conversion failed: '
+        );
+        res
+          .status(415)
+          .json(
+            createErrorResponse(
+              415,
+              `a ${fragment.mimeType} fragment cannot be returned as a ${mimeType}`
+            )
+          );
+      }
+    }
 
-    const data = await fragment.getData();
-    res.setHeader('Content-Type', fragment.type);
     res.status(200).send(data);
   } catch (error) {
-    logger.debug(`ownerId=${req.user}, id=${req.params.id}`);
+    logger.debug({ ownerId: req.user, idExt: req.params.id }, 'Provided information: ');
     res.status(404).json(createErrorResponse(404, 'The fragment does not exist'));
   }
 };
