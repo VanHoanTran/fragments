23cde10d07456a3fb31f24944c9566b0223c6266
VanHoanTran <tvan-hoan@myseneca.ca>
2022-11-11 07:17:53 -0500

    add logic to get an array of fragment metadata or just ids

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -1,11 +1,29 @@
 // src/routes/api/get.js
+const { Fragment } = require('../../model/fragment');
+const { createSuccessResponse, createErrorResponse } = require('../../response.js');
+const logger = require('../../logger');
 
-const {createSuccessResponse} = require('../../response.js');
 /**
  * Get a list of fragments for the current user
  */
- var data = {fragments:[]};
- module.exports = (req, res) => {
-    // TODO: this is just a placeholder to get something working...
-    res.status(200).json(createSuccessResponse(data));
-  };
+
+module.exports = async (req, res) => {
+  // TODO: this is just a placeholder to get something working...
+  logger.debug({ query: req.query }, 'Request Query: ');
+  let expand;
+  try {
+    expand = req.query.expand && req.query.expand === '1' ? true : false;
+    logger.debug({ expand }, 'Expand: ');
+    const fragments = await Fragment.byUser(req.user, expand);
+    res.status(200).json(
+      createSuccessResponse({
+        fragments: fragments,
+      })
+    );
+    logger.debug({ fragments }, 'Get successfully: ');
+  } catch (err) {
+    logger.debug({ err }, 'Error Message');
+    logger.debug({ user: req.user, expand }, 'Information received: ');
+    res.status(500).json(createErrorResponse(500, err));
+  }
+};
