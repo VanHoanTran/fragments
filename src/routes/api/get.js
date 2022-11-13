b14c67335ca4ee0d6c9c0b82f4a48d175fc1f6d3
VanHoanTran <tvan-hoan@myseneca.ca>
2022-11-13 09:07:19 -0500

    remove unnecessary try-catch

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -1,6 +1,6 @@
 // src/routes/api/get.js
 const { Fragment } = require('../../model/fragment');
-const { createSuccessResponse, createErrorResponse } = require('../../response.js');
+const { createSuccessResponse } = require('../../response.js');
 const logger = require('../../logger');
 
 /**
@@ -11,19 +11,13 @@ module.exports = async (req, res) => {
   // TODO: this is just a placeholder to get something working...
   logger.debug({ query: req.query }, 'Request Query: ');
   let expand;
-  try {
-    expand = req.query.expand && req.query.expand === '1' ? true : false;
-    logger.debug({ expand }, 'Expand: ');
-    const fragments = await Fragment.byUser(req.user, expand);
-    res.status(200).json(
-      createSuccessResponse({
-        fragments: fragments,
-      })
-    );
-    logger.debug({ fragments }, 'Get successfully: ');
-  } catch (err) {
-    logger.debug({ err }, 'Error Message');
-    logger.debug({ user: req.user, expand }, 'Information received: ');
-    res.status(500).json(createErrorResponse(500, err));
-  }
+  expand = req.query.expand && req.query.expand === '1' ? true : false;
+  logger.debug({ user: req.user, expand }, 'Information received: ');
+  const fragments = await Fragment.byUser(req.user, expand);
+  res.status(200).json(
+    createSuccessResponse({
+      fragments: fragments,
+    })
+  );
+  logger.debug({ fragments }, 'Get successfully: ');
 };
