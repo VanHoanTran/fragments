e4a2f1ea903c769c492a61e8d49e6525edae8819
VanHoanTran <tvan-hoan@myseneca.ca>
2022-11-13 07:49:32 -0500

    add tests for type conversion

--- tests/unit/get-by-id.test.js
+++ tests/unit/get-by-id.test.js
@@ -43,6 +43,7 @@ describe('GET /v1/fragments/:id', () => {
 
     expect(res.statusCode).toBe(404);
   });
+
   //requests without authentication are rejected with code 401
   test('requests without authentication should return HTTP 401 response', () =>
     request(app).get('/v1/fragments/id').expect(401));
@@ -53,4 +54,107 @@ describe('GET /v1/fragments/:id', () => {
       .get('/v1/fragments/id')
       .auth('InvalidUsername@email.com', 'InvalidPassword')
       .expect(401));
+});
+
+describe('GET /v1/fragments/:id.ext', () => {
+  //
+  test('A text/markdown can be converted to text/plain type', async () => {
+    const postRes = await request(app)
+      .post('/v1/fragments')
+      .auth('user1@email.com', 'password1')
+      .set('Content-Type', 'text/markdown')
+      .send('# markdown text test');
+
+    const id = JSON.parse(postRes.text).fragment.id;
+
+    const res = await request(app)
+      .get(`/v1/fragments/${id}.txt`)
+      .auth('user1@email.com', 'password1');
+
+    expect(res.get('Content-Type')).toEqual('text/plain');
+  });
+
+  // A text/markdown can be converted to text/html type
+  test('A text/markdown can be converted to text/html type', async () => {
+    const postRes = await request(app)
+      .post('/v1/fragments')
+      .auth('user1@email.com', 'password1')
+      .set('Content-Type', 'text/markdown')
+      .send('# Markdown test');
+
+    const id = JSON.parse(postRes.text).fragment.id;
+
+    const res = await request(app)
+      .get(`/v1/fragments/${id}.html`)
+      .auth('user1@email.com', 'password1');
+
+    expect(res.get('Content-Type')).toEqual('text/html');
+  });
+
+  // A text/markdown can be converted to text/html type
+  test('A text/markdown can be converted to text/html type', async () => {
+    const postRes = await request(app)
+      .post('/v1/fragments')
+      .auth('user1@email.com', 'password1')
+      .set('Content-Type', 'text/markdown')
+      .send('# Markdown test');
+
+    const id = JSON.parse(postRes.text).fragment.id;
+
+    const res = await request(app)
+      .get(`/v1/fragments/${id}.html`)
+      .auth('user1@email.com', 'password1');
+
+    expect(res.get('Content-Type')).toEqual('text/html');
+  });
+
+  // A text/markdown can be converted to text/html type
+  test('The fragment should be rendered as html format', async () => {
+    const postRes = await request(app)
+      .post('/v1/fragments')
+      .auth('user1@email.com', 'password1')
+      .set('Content-Type', 'text/markdown')
+      .send('# Markdown test');
+
+    const id = JSON.parse(postRes.text).fragment.id;
+
+    const res = await request(app)
+      .get(`/v1/fragments/${id}.html`)
+      .auth('user1@email.com', 'password1');
+
+    expect(res.text).toEqual('<h1>Markdown test</h1>\n');
+  });
+
+  // The fragment should be rendered as markdown
+  test('The fragment should be rendered as markdown', async () => {
+    const postRes = await request(app)
+      .post('/v1/fragments')
+      .auth('user1@email.com', 'password1')
+      .set('Content-Type', 'text/markdown')
+      .send('# Markdown test');
+
+    const id = JSON.parse(postRes.text).fragment.id;
+
+    const res = await request(app)
+      .get(`/v1/fragments/${id}.md`)
+      .auth('user1@email.com', 'password1');
+
+    expect(res.text).toEqual('# Markdown test');
+  });
+
+  test('unsupported type conversion should return HTTP 415 response', async () => {
+    const postRes = await request(app)
+      .post('/v1/fragments')
+      .auth('user1@email.com', 'password1')
+      .set('Content-Type', 'text/markdown')
+      .send('# Markdown test');
+
+    const id = JSON.parse(postRes.text).fragment.id;
+
+    const res = await request(app)
+      .get(`/v1/fragments/${id}.png`)
+      .auth('user1@email.com', 'password1');
+
+    expect(res.statusCode).toBe(415);
+  });
 });
