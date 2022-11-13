7e4aa03dc8faca6e25325758a0ce81dd9a51b753
VanHoanTran <tvan-hoan@myseneca.ca>
2022-11-13 07:51:52 -0500

    fixing reponse code

--- src/routes/api/get-by-id-info.js
+++ src/routes/api/get-by-id-info.js
@@ -5,20 +5,14 @@ module.exports = async (req, res) => {
   try {
     logger.debug({ ownerId: req.user, fragmentsId: req.params.id }, 'Provided information: ');
     const frag = await Fragment.byId(req.user, req.params.id);
-    if (!frag) {
-      logger.debug({ fragmentsId: req.params.id }, 'Fragment not found: ');
-      return res
-        .status(404)
-        .json(createErrorResponse(404, 'Theres no fragment related to this id'));
-    }
     res.status(200).json(
       createSuccessResponse({
         fragment: frag,
       })
     );
   } catch (err) {
+    logger.debug({ fragmentsId: req.params.id }, 'Fragment not found: ');
     logger.debug({ err }, 'Error Message');
-    logger.debug({ ownerId: req.user, fragmentsId: req.params.id }, 'Information received: ');
-    res.status(500).json(createErrorResponse(500, err.message));
+    res.status(404).json(createErrorResponse(404, 'Theres no fragment related to this id'));
   }
 };
