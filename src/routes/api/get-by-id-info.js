34d29686d50b958bef34b020a61077fb5814b810
VanHoanTran <tvan-hoan@myseneca.ca>
2022-11-23 23:25:24 -0500

    fix typo

--- src/routes/api/get-by-id-info.js
+++ src/routes/api/get-by-id-info.js
@@ -13,6 +13,6 @@ module.exports = async (req, res) => {
   } catch (err) {
     logger.debug({ fragmentsId: req.params.id }, 'Fragment not found: ');
     logger.debug({ err }, 'Error Message');
-    res.status(404).json(createErrorResponse(404, 'Theres no fragment related to this id'));
+    res.status(404).json(createErrorResponse(404, 'There is no fragment related to this id'));
   }
 };
